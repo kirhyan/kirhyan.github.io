c17e8c6c34f98beb479e3a3ad917ba49d339f7e7
Miriam Blanco <m.blancoga@gmail.com>
2024-10-28 13:43:07 +0100

    Fixed routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Layout from "./routes/Layout.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import About from "./routes/About.tsx";
 import Resume from "./routes/Resume.tsx";
 import Projects from "./routes/Projects.tsx";
 import Home from "./routes/Home.tsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
